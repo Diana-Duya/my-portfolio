b4dda8fd0d76c6241407263259d3b766f355bf25
unknown <dianaduya2@gmail.com>
2022-09-16 14:30:05 +0100

    Add email validation

--- script.js
+++ script.js
@@ -50,3 +50,26 @@ closeButton.addEventListener('click', () => {
   popUp.classList.add('popup1');
   blurry.style = 'display : none';
 });
+
+
+const form = document.querySelector('.form')
+const email = document.querySelector('#email')
+const error = document.querySelector('.error')
+
+function checkEmail(Diana) {
+    if (Diana.match(/^([a-z0-9_\.\-])+\@(([a-z0-9\-])+\.)+([a-z0-9]{2,4})+$/)) {
+        return true;
+}
+    return false;
+}
+
+form.addEventListener('submit', (e) => {
+if (checkEmail(email.value)) {
+    error.textContent = '';
+}else {
+    e.preventDefault();
+    error.textContent ="Enter a valid email";
+}
+});
+
+
